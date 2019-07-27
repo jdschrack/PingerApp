d4b91df84a5775d7321651e86cf1070a5408ee5c
Jeremey Schrack <jeremey@securegive.com>
2019-07-27 12:46:25 -0400

    adding files

---
+++ PingerApp/Module1.vb
@@ -0,0 +1,48 @@
+﻿Imports System.IO
+Imports System.Xml
+Imports System.Xml.Serialization
+Imports System.Net
+Imports System.Text
+Module Module1
+    Dim s As New StreamReader("C:\temp\servers.txt")
+    Sub Main()
+        Dim o As New StreamWriter("c:\temp\servers_out.csv", False)
+        Dim line As String
+        line = s.ReadLine
+
+        Do While (Not line Is Nothing)
+            Dim host As String = line
+            PingMachine(host)
+            line = s.ReadLine
+        Loop
+        o.Close()
+
+
+    End Sub
+    Private Sub PingMachine(ByVal System As String)
+
+        'At every tick of the timer (depending on what you set the timer to)
+        'it executes this code
+        Dim hostString As String = System
+        Dim pingResults As String
+        Dim i As Integer = 0
+        Try
+
+            'ping the host and set the results variabl
+            pingResults = My.Computer.Network.Ping(hostString, 300)
+
+            If pingResults = True Then
+
+                'if it was a previously dead host, alert the user it's back online
+                'and reset the dead variable back to 0
+                Console.WriteLine(System & " - Is Online")
+            Else
+                Console.WriteLine(System & " - Did not respond")
+            End If
+        Catch Ex As Exception
+
+        End Try
+
+    End Sub
+
+End Module
---
+++ PingerApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PingerApp")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("UnitedHealth Group")> 
+<Assembly: AssemblyProduct("PingerApp")> 
+<Assembly: AssemblyCopyright("Copyright © UnitedHealth Group 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("14ceb290-6b68-487d-9700-04d08c3476b5")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
